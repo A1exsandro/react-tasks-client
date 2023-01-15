356a644db015a84ecf95222881683fa0b64d325f
A1exsandro <alexsandro.acp@gmail.com>
2023-01-15 16:43:18 -0400

    submit task for api

--- src/components/tasks/Tasks.js
+++ src/components/tasks/Tasks.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import List from './list/List';
+import CreateTask from './create_tasks/CreateTasks';
+import Button from 'react-bootstrap/Button';
 
 class Tasks extends Component {
 	constructor(props) {
@@ -27,11 +29,13 @@ class Tasks extends Component {
 				<Col xs={{ span: 8, offset: 2 }} className="tasks_list">
 					<p className="title">To-do</p>
 					<List loadTasks={this.loadTasks} tasks={ this.state.tasks.filter((task) => task.done != true ) }/>
+					<CreateTask loadTasks={this.loadTasks}/>
 				</Col>
 				<Col xs={{ span: 8, offset: 2 }} className="tasks_list">
 					<p className="title">Done</p>
 					<List loadTasks={this.loadTasks} tasks={ this.state.tasks.filter((task) => task.done == true ) }/>
-				</Col>
+					<Button variant="red" className="float-right remove_tasks_btn">Remove all tasks</Button>
+ 				</Col>
 			</Row>
 		);
 	}
