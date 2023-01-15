f18d8c4630c84ae2c839f82902fa0e347cf13b25
A1exsandro <alexsandro.acp@gmail.com>
2023-01-15 13:27:04 -0400

    refactor

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function Header() {
-  return (
-    <div>
-      <Navbar>
-        <Navbar.Brand>
-          <FontAwesomeIcon icon="check-circle" size="lg"/> Task Finisher
-        </Navbar.Brand>
-      </Navbar>
-    </div>
-  );
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar>
+          <Navbar.Brand>
+            <FontAwesomeIcon icon="check-circle" size="lg"/> Task Finisher
+          </Navbar.Brand>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default Header;
